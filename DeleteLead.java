ca26e3eff95abced16cc73cee469df60cad57308
julietrajam <44438970+julietrajam@users.noreply.github.com>
2021-09-21 01:15:11 +0530

    Update DeleteLead.java

--- DeleteLead.java
+++ DeleteLead.java
@@ -29,38 +29,54 @@ public class DeleteLead {
 			driver.findElement(By.xpath("//input[@value='Login']")).click();
 
 			driver.findElement(By.xpath("//a[contains(text(),'CRM/SFA')]")).click();
+			
+			//Click Leads
 
 			driver.findElement(By.linkText("Leads")).click();
-
+			
+            //Click Find Leads
 			driver.findElement(By.linkText("Find Leads")).click();
-
+			
+			//Click Phone
 			driver.findElement(By.xpath("//span[text()='Phone']")).click();
-
+            
+			
+			//Click Country Code
 			WebElement countryCode = driver.findElement(By.xpath("//input[@name='phoneCountryCode']"));
 
 			countryCode.clear();
 			countryCode.sendKeys("4");
 
+			//Edit Areacode
 			driver.findElement(By.xpath("//input[@name='phoneAreaCode']")).sendKeys("64");
 
+			//Enter Phone Number
 			driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("1234567899");
 
+			//Click Find Leads
 			driver.findElement(By.linkText("Find Leads")).click();
 
+			//Click First Lead
 			WebElement deleteRow = driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-partyId'])[1]/a"));
 			String deleteData = deleteRow.getText();
 			System.out.println("Deleted Data:" + deleteData);
 
 			deleteRow.click();
-
+			
+			
+			//Click Delete
 			driver.findElement(By.linkText("Delete")).click();
 
+			//Click Find Leads
 			driver.findElement(By.linkText("Find Leads")).click();
 
 			// driver.switchTo().frame(org.opentaps.gwt.crmsfa.crmsfa);
-
+			
+			
+            //Passing the deleted lead id
 			driver.findElement(By.name("id")).sendKeys(deleteData);
-
+			
+			//Click Find Leads
 			driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
 
 			WebElement message = driver.findElement(By.xpath("//div[text()='No records to display']"));
@@ -79,4 +95,6 @@ public class DeleteLead {
 	
 
 	}
+
+
 
